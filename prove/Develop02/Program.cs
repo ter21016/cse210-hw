7c7f064b2ccba44d1e6ad47ea893c70116894f7d
ter21016 <blackblue58@hotmail.com>
2024-03-07 20:29:19 -0800

    add Code

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+     
+        // loop that will keep on asking if the user doent quit.
+        //menu on 5 options
+        bool quit = false;
+        while (!quit) 
+        {
+            Console.WriteLine("PLease select one of the following choices");
+            Console.WriteLine("1.Write\n2.Display\n3.load\n4.save\n5.Quit");
+            string user_respons = Console.ReadLine();
+            switch (user_respons) {
+                case "1":{
+                    string prompt = "randon question";
+                    Console.WriteLine(prompt);
+                    string user_answer = Console.ReadLine();
+                    DateTime time = DateTime.Now;
+                   
+                     break;
+                };
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                
+            
+            }
+        
+
+        }
+        //write
+        //display
+        //load
+        //save
+        //quit
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using prepare/Learning
 
 class Program
 {
